06559fc30812f412fb27cbe292f2ccb33c70bd93
hsk2ad <hsk2ad@virginia.edu>
2021-06-14 15:04:38 -0400

    First Java Program

---
+++ JavaProgram.java
@@ -0,0 +1,11 @@
+
+public class JavaProgram {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Sam Kim");
+		System.out.println("I'm 23 years old");
+		System.out.println("My hometown is DC");
+
+	}
+
+}
