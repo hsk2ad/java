dd28592461dc47aa10ca0572e236076a1ccf9134
hsk2ad <hsk2ad@virginia.edu>
2021-06-14 23:02:49 -0400

    Pythagorean

---
+++ Pythagorean.java
@@ -0,0 +1,7 @@
+public class Pythagorean {
+	public double calculateHypotenuse(int legA, int legB) {
+		double total = Math.sqrt(legA * legA + legB * legB);
+		return total;
+	}
+	
+}
---
+++ PythagoreanTest.java
@@ -0,0 +1,11 @@
+
+public class PythagoreanTest {
+
+	public static void main(String[] args) {
+		Pythagorean example1 = new Pythagorean();
+		double answer = example1.calculateHypotenuse(3, 4);
+		System.out.println(answer);
+
+	}
+
+}
