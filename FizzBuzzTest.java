8a2f53ff8f511000a9dd8facfdfd1d467ffef06d
hsk2ad <hsk2ad@virginia.edu>
2021-06-14 23:38:27 -0400

    FizzBuzz

---
+++ FizzBuzzTest.java
@@ -0,0 +1,11 @@
+
+public class FizzBuzzTest {
+
+	public static void main(String[] args) {
+		FizzBuzz test1 = new FizzBuzz();
+		String answer = test1.FizzBuzz(13);
+		System.out.println(answer);;
+
+	}
+
+}
