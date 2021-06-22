05fc26b8ef3abee448aa96716cb2038bf3d235fc
hsk2ad <hsk2ad@virginia.edu>
2021-06-21 21:48:38 -0400

    Human

---
+++ HumanTest.java
@@ -0,0 +1,12 @@
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Human h = new Human(100);
+		h.attack(h);
+		
+		System.out.println(h.health);
+	}
+
+}
